d0fbbf36997a2f2869f05f6965965bc26172545b
YoonSungLee <nowhere3070@naver.com>
2022-11-10 08:19:02 +0900

    11.09

--- Yoonsung/기둥과 보 설치.py
+++ Yoonsung/기둥과 보 설치.py
@@ -8,7 +8,15 @@ def possible(answer):
     for x, y, stuff in answer:
         if stuff == 0:  # 설치된 것이 '기둥'인 경우
             # '바닥 위' 혹은 '보의 한쪽 끝부분 위' 혹은 '다른 기둥 위'라면 정상
-            if y == 0 or [x - 1, y, 1] in answer or [x, y, 1]
+            if y == 0 or [x - 1, y, 1] in answer or [x, y, 1] in answer or [x, y - 1, 0] in answer:
+                continue
+            return False    # 아니라면 거짓(False) 반환
+        elif stuff == 1:    # 설치된 것이 '보'인 경우
+            # '한쪽 끝부분이 기둥 위' 혹은 '양쪽 끝부분이 다른 보와 동시에 연결'이라면 정상
+            if [x, y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or ([x - 1, y, 1] in answer and [x + 1, y, 1] in answer):
+                continue
+            return False    # 아니라면 거짓(False) 반환
+    return True
 
 
 def solution(n, build_frame):
