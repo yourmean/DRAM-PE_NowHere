b9ab14369cbcf78fa960c68cb0fb0997bd569dcc
Yumin Lee <55529646+yourmean@users.noreply.github.com>
2023-06-29 21:31:03 +0900

    Update Q15_특정거리의도시찾기.py

--- Yumin/algorithm/Q15_특정거리의도시찾기.py
+++ Yumin/algorithm/Q15_특정거리의도시찾기.py
@@ -3,20 +3,20 @@
 from collections import deque
 
 # 도시의 개수, 도로의 개수, 거리 정보, 출발 도시 번호
-n, m, k, x = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
+a, b, c, d = map(int, input().split())
+graph = [[] for _ in range(a + 1)]
 
 # 모든 도로정보 입력받기
-for _ in range(m):
+for _ in range(b):
     a, b = map(int, input().split())
     graph[a].append(b)
 
 # 모든 도시에 대한 최단거리 초기화
-distance = [-1] * (n + 1)
-distance[x] = 0 # 출발 도시까지의 거리 = 0으로 설정
+distance = [-1] * (a+1)
+distance[d] = 0 # 출발 도시까지의 거리 = 0으로 설정
 
 # BFS 수행
-q = deque([x])
+q = deque([d])
 while q:
     now = q.popleft()
     # 현재 도시에서 이동할 수 있는 모든 도시 확인
@@ -27,12 +27,12 @@ while q:
             q.append(next_node)
 
 # 최단 거리가 K인 모든 도시의 번호를 오름차순으로 출력
-check = False
-for i in range(1, n + 1):
-    if distance[i] == k:
+target = False
+for i in range(1, a+1):
+    if distance[i] == c:
         print(i)
-        check = True
+        target = True
 
 # 최단 거리가 K인 도시가 없는 경우
-if check == False:
+if target == False:
     print(-1)
