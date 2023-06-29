b76415c68329dec5cbbc2b0d8ee5d941b8ba2485
Yumin Lee <55529646+yourmean@users.noreply.github.com>
2023-06-29 21:23:01 +0900

    Update Q15_특정거리의도시찾기.py

--- Yumin/algorithm/Q15_특정거리의도시찾기.py
+++ Yumin/algorithm/Q15_특정거리의도시찾기.py
@@ -6,24 +6,23 @@ from collections import deque
 n, m, k, x = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
 
-# 모든 도로 정보 입력 받기
+# 모든 도로정보 입력받기
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 
-# 모든 도시에 대한 최단 거리 초기화
+# 모든 도시에 대한 최단거리 초기화
 distance = [-1] * (n + 1)
-distance[x] = 0 # 출발 도시까지의 거리는 0으로 설정
+distance[x] = 0 # 출발 도시까지의 거리 = 0으로 설정
 
-# 너비 우선 탐색(BFS) 수행
+# BFS 수행
 q = deque([x])
 while q:
     now = q.popleft()
-    # 현재 도시에서 이동할 수 있는 모든 도시를 확인
+    # 현재 도시에서 이동할 수 있는 모든 도시 확인
     for next_node in graph[now]:
-        # 아직 방문하지 않은 도시라면
+        # 아직 방문하지 않은 도시인 경우 최단 거리 갱신
         if distance[next_node] == -1:
-            # 최단 거리 갱신
             distance[next_node] = distance[now] + 1
             q.append(next_node)
 
@@ -34,6 +33,6 @@ for i in range(1, n + 1):
         print(i)
         check = True
 
-# 만약 최단 거리가 K인 도시가 없다면, -1 출력
+# 최단 거리가 K인 도시가 없는 경우
 if check == False:
     print(-1)
